498ef4e7024edf4de819f8d710d713a83ceba8bf
J. SUJAN CHOWDARY <91127394+Jsujanchowdary@users.noreply.github.com>
2025-09-04 21:01:42 +0530

    Update setupfile.groovy

--- vars/setupfile.groovy
+++ vars/setupfile.groovy
@@ -1,16 +1,14 @@
-def cloneSource = {
+def cloneSource() {
     stage('Clone Source') {
         echo 'This stage is cloning a git repository from the shared library.'
         sh 'echo "Running git clone command..."'
     }
 }
 
-def buildApplication = {
+def buildApplication() {
     stage('Build') {
         echo 'This stage is for building the application.'
         sh 'echo "Running build scripts..."'
         sh 'echo "Build complete!"'
     }
 }
-
-return this
